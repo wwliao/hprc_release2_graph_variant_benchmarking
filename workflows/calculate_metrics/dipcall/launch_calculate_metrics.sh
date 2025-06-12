010cfc0024320a0320e8b0dd5dfe173142930de7
Wen-Wei Liao <wen-wei.liao@yale.edu>
2025-06-12 15:46:21 -0400

    Add workflow to calculate Dipcall performance metrics

---
+++ workflows/calculate_metrics/dipcall/launch_calculate_metrics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=launch_calculate_metrics
+#SBATCH --output=launch_calculate_metrics-%j.out
+#SBATCH --partition=pi_hall
+#SBATCH --constraint=nogpu
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=5G
+#SBATCH --time=1-00:00:00
+
+module purge
+module load Nextflow/24.04.4
+
+SAMPLE_SHEET="samplesheet.csv"
+GRAPH="dipcall"
+OUTDIR="results"
+
+nextflow run main.nf \
+    -ansi-log true \
+    -profile mccleary \
+    --sample_sheet ${SAMPLE_SHEET} \
+    --graph ${GRAPH} \
+    --outdir ${OUTDIR}
